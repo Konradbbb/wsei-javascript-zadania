9a1adc20f149e5ce26b7424693e6df2f0f5e65b0
Konrad <49588583+Konradbbb@users.noreply.github.com>
2020-10-04 13:35:13 +0200

    Update script.js

---
+++ 3.Operatory/script.js
@@ -0,0 +1,27 @@
+//Zadanie 1:
+
+let isEmpty = false;
+let isGold = true;
+console.log(isEmpty == isGold);
+
+
+//Zadanie 2:
+let firstVariable = 4;
+let secondVariable = 3;
+let moduloResult = 0;
+moduloResult = firstVariable % secondVariable;
+console.log(moduloResult);
+
+
+//Zadanie 3:
+var someNumber = "4";
+var someString = "425";
+let stringsResult = "";
+stringsResult = someNumber + someString;
+console.log(stringsResult);
+
+//Zadanie 4:
+var someNumber = 425;
+var someString = "425";
+console.log(someNumber == someString)
+console.log(someNumber === someString)
