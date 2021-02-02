dea00ffbc00092ec2ab54a8f0efc70a8ae4f9631
Konrad <49588583+Konradbbb@users.noreply.github.com>
2021-02-02 18:44:19 +0100

    Update script.js

--- 11.DOM-wyszukiwanie/script.js
+++ 11.DOM-wyszukiwanie/script.js
@@ -1,111 +1,23 @@
-//Zadanie 1 
+// Zadanie 1
 
-const buz = document.querySelector('#buz');
+var findListElements = document.querySelectorAll(".list");
 
-const buzParent = buz.parentElement;
-console.log(buzParent);
+console.log(findListElements);
 
-const baz = document.querySelector('#baz');
+// Zadanie 2 
 
-const bazBrother =baz.previousElementSibling;
-
-console.log(bazBrother);
-
-const foo = document.querySelector('#foo');
-
-const fooChildren = foo.children;
-console.log(fooChildren);
-
-const fooParent = foo.parentElement;
-
-console.log(fooParent);
-
-const fooFirstChild =foo.firstElementChild;
-
-console.log(fooFirstChild);
-
-const fooMiddleChild = foo.children[1]
-
-
-//Zadanie 2 
-
-const Listener= (e)=>{
-    e.addEventListener("click", function(){
-        console.log(this.firstElementChild.innerText.trim());
-    });
-};
-
-Listener(document.getElementById("ex2"));
-
-//Zadanie 3 
-
-const buttons = document.querySelectorAll("#ex3 div button");
-
-buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-       const span = button.nextElementSibling;
-        if(span.style.display === "none") {
-            span.style.display = "block";
-        } else {
-            span.style.display = "none";
-        }
-    });
-})
-
-//Zadanie 4 
-
-const buttons = document.querySelectorAll("#ex3 div button");
-
-buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-        var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
-        button.parentElement.style.backgroundColor = randomColor;
-    });
-})
-
-//Zadanie 5
-
-var list = document.querySelector("div#ex5 ul");
-var divs = document.querySelectorAll("div#ex5 div");
-let allLi = document.querySelectorAll("div#ex5 li");
-var firstLi = allLi[0];
-var lastLi = allLi[allLi.length - 1];
-let oddElements = [];
-allLi.forEach((element, index) => {
-    if(index% 2 !== 0) oddElements.push(element);
-});
-
-for(let i=0; i<divs.length; i++){
-    divs[i].addEventListener("mouseover", function() {
-        const color = this.style.backgroundColor;
-        //firstLi.style.backgroundColor = color; 
-        //lastLi.style.backgroundColor = color;
-        
-        /*oddElements.forEach((element) => {
-            element.style.backgroundColor = color;
-        });*/
-        
-      /*  allLi.forEach((element) => {
-            element.style.backgroundColor = color;
-        }); */
-        list.style.backgroundColor = color;
-        
-    });
-
+returnElements = (tag) =>{
+    return document.querySelectorAll(tag);
 }
 
-//Zadanie 6 
+console.log(returnElements("body"));
 
-const mainDiv = document.getElementById('ex6');
+// Zadanie 3
 
-let one = mainDiv.firstElementChild.firstElementChild.firstElementChild;
+const elem = document.getElementById('list');
 
-let two = mainDiv.firstElementChild.parentElement.firstElementChild.firstElementChild.nextElementSibling.parentElement;
+// Zadanie 4
 
-let three = mainDiv.parentElement.firstElementChild.parentElement.children[1].firstElementChild.firstElementChild.firstElementChild;
-
-
-console.log(one);
-console.log(two);
-console.log(three);
-
+const getElements = (selector) {
+    return document.querySelectorAll(selector);
+}
