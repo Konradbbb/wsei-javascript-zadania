44aba08012c4c2bb3cf21a09563942f18ebea786
Konrad <49588583+Konradbbb@users.noreply.github.com>
2021-02-05 23:56:23 +0100

    Update script.js

---
+++ 1.Początki/script.js
@@ -0,0 +1,15 @@
+// Zadanie 1:
+
+console.log("Konrad B");
+
+// Zadanie 2:
+
+console.log(1 + 2);
+console.log(32 / 16);
+console.log(5 * 2);
+
+// Zadanie 3:
+
+console.log("Hello World");
+console.log(Hello World);
+console.log("Hello World);
